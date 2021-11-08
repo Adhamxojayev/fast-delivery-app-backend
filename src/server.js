9e121899247ee1a6df1e779a2f47653c98d6af3a
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-11-08 11:55:38 +0500

    database updated

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express()
 app.use((req,res,next) => {
     res.setHeader('Access-Control-Allow-Origin','*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Access-Control-Allow-Origin');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST, DELETE, PUT');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
     next()
 })
 
