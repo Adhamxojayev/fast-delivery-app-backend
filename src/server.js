7cdc927dc710d2439d2de4fd1e7e2112f1cbf74d
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-11-09 10:38:58 +0500

    cors added

--- src/server.js
+++ src/server.js
@@ -2,17 +2,11 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 import { PORT } from './config.js'
 import router from './modules/index.js'
+import cors from 'cors'
 
 const app = express()
 
-
-app.use((req,res,next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, Accept, Content-Type, Access-Control-Allow-Origin');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
-    next()
-})
-
+app.use(cors())
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
