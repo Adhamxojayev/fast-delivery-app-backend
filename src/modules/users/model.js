62f5bbf936ab7955afe73a055431b046f8189788
Ahror Adhamxojayev <axroradhamxojayev@gmail.com>
2021-11-10 14:17:57 +0500

    user router added

--- src/modules/users/model.js
+++ src/modules/users/model.js
@@ -3,7 +3,14 @@ import fetch from '../../lib/postgres.js'
 export default {
     get: async () => {
         try {
-            return await fetch(true, `select * from users`)
+            return await fetch(true, `select 
+                u.id,
+                u.name,
+                u.phone,
+                count(o.id) as count
+            from users u
+            join orders o on o.user_id = u.id
+            group by u.name, u.phone,u.id`)
         } catch (error) {
             console.error(error);
         }
